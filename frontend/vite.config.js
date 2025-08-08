c6bae7940fe5c407d14e25ff8daea305c7ed7730
nalbano4 <nalbano4@gmail.com>
2025-08-08 12:25:34 -0400

    rewite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/',  // optional; default is '/'
+  base: './', // critical so built HTML uses relative asset paths
   build: { outDir: 'dist', assetsDir: 'assets' }
 })
